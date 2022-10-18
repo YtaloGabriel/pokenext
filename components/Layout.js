d8be9194999f514a641790594ca21b0b19a2fc18
ytalogabr <ytalogabr@gmail.com>
2022-10-18 03:40:31 -0300

    SEO optimization

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,17 @@ const Layout = ({ children }) => {
   return (
     <div className="content">
       <Head>
+        <meta
+          name="description"
+          content="Lista de Pokémons utilizando Next.js"
+        />
+        <meta
+          name="keywords"
+          content="Ytalo Gabriel, Next.js, Next, Ytalo, Gabriel, site, projeto, github, vercel, deploy, web, desenvolvedor, programador, @ytalogabr, ytalogabr, ytalo, javascript, css, html, pokemon, pokémon, api, link, spa, react, react js, js, linkedin, maceió, alagoas, brasil"
+        />
+        <meta name="robots" content="index, follow" />
         <link rel="shortcut icon" href="/images/pokeball.ico" />
-        <title>PokeNext - Home</title>
+        <title>PokeNext</title>
       </Head>
 
       <Header />
